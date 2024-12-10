dafbed0f697b17fe83441bb7a3bacf21f15d05f7
upeshchalise <upeshchalise@gmail.com>
2024-12-10 22:08:10 +0545

    withdraw dunction

--- bankapi/main.go
+++ bankapi/main.go
@@ -24,6 +24,7 @@ func main() {
 
 	http.HandleFunc("/statement", statement)
 	http.HandleFunc("/deposit", deposit)
+	http.HandleFunc("/withdraw", withdraw)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -81,4 +82,39 @@ func deposit(w http.ResponseWriter, r *http.Request) {
 		}
 
 	}
+}
+
+func withdraw(w http.ResponseWriter, r *http.Request) {
+	numberqs := r.URL.Query().Get("number")
+	amountqs := r.URL.Query().Get("amount")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing")
+		return
+	}
+
+	if amountqs == "" {
+		fmt.Fprintf(w, "Amount number is missing")
+		return
+	}
+
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid amount number")
+	} else {
+		account, ok := accounts[number]
+
+		if !ok {
+			fmt.Println(w, "Account with number %v can't be found", number)
+		} else {
+			err := account.Withdraw(amount)
+			if err != nil {
+				fmt.Println(w, "%v", err)
+			} else {
+				fmt.Fprintln(w, account.Statement())
+			}
+		}
+	}
+
 }
